5177a5f8e39601bc74d60623e40869d5a83c62f8
Nadhif-Sk <Nadifshohibulk@gmail.com>
2021-03-26 18:48:04 +0700

    first commit

---
+++ database/migrations/2021_02_16_043936_create_transaksi_masuk_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiMasukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('transaksi_masuk')){
+            Schema::create('transaksi_masuk', function (Blueprint $table) {
+                $table->Increments('kd_transaksi_masuk');
+                $table->unsignedInteger('kd_produk');
+                $table->foreign('kd_produk')->references('kd_produk')->on('kd_produk');
+                $table->unsignedInteger('kd_supplier');
+                $table->foreign('kd_supplier')->references('kd_supplier')->on('kd_supplier');
+                $table->date('tgl_transaksi');
+                $table->integer('jumlah');
+                $table->integer('harga');
+                $table->timestamps();
+            });
+        }
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transaksi_masuk',function(Blueprint $table){
+            $table->dropForeign(['kd_produk']);
+            $table->dropForeign(['kd_supplier']);
+        });
+        
+        Schema::dropIfExists('transaksi_masuk');
+    }
+}
